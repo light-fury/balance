3e983231cf811b1029e9495e8ae6f09f73a1dc0f
light-fury <leech.developer@gmail.com>
2022-08-20 12:55:26 +0200

    initial commit

---
+++ contracts/TakepileVault.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.7.5;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+contract TakePileVault is Ownable, AccessControl {
+    using SafeMath for uint256;
+
+}
