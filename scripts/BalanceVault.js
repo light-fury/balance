3b600d4b596684b76276147ec88c976bfd7925a7
light-fury <leech.developer@gmail.com>
2022-08-30 00:16:32 +0200

    balance pass manager
    Signed-off-by: pwntr0n <pwntr0n@fantohm.com>

--- scripts/BalanceVault.js
+++ scripts/BalanceVault.js
@@ -12,11 +12,11 @@ async function main() {
         usdbAddress,
     } = require(`./networks-${network}.json`);
 
-    const BalanceVaultTemplate = await ethers.getContractFactory('BalanceVault');
-    // const balanceVaultTemplate = await BalanceVaultTemplate.attach("0x0f3a452cfF79bB4a97c210DB6D2A59F85986CAA9");
-    const balanceVaultTemplate = await BalanceVaultTemplate.deploy();
-    console.log(`Deployed BalanceVaultTemplate to: ${balanceVaultTemplate.address}`);
-    console.log(`\nVerify:\nnpx hardhat verify --network ${network} ${balanceVaultTemplate.address}`);
+    // const BalanceVaultTemplate = await ethers.getContractFactory('BalanceVault');
+    // // const balanceVaultTemplate = await BalanceVaultTemplate.attach("0x0f3a452cfF79bB4a97c210DB6D2A59F85986CAA9");
+    // const balanceVaultTemplate = await BalanceVaultTemplate.deploy();
+    // console.log(`Deployed BalanceVaultTemplate to: ${balanceVaultTemplate.address}`);
+    // console.log(`\nVerify:\nnpx hardhat verify --network ${network} ${balanceVaultTemplate.address}`);
 
     // const BalanceVaultShareTemplate = await ethers.getContractFactory('BalanceVaultShare');
     // // const balanceVaultShareTemplate = await BalanceVaultShareTemplate.attach("0x4bF4a98a8A96b2270D877cA9af035715F2A128aB");
@@ -38,6 +38,22 @@ async function main() {
     //
     // console.log(`\nVerify:\nnpx hardhat verify --network ${network} `+
     //     `${balanceVaultManager.address} "${daoAddress}" "${usdbAddress}" ${feeBorrower} ${feeLenderUsdb} ${feeLenderOther}`);
+
+
+    // const BalancePassManager = await ethers.getContractFactory('BalancePassManager');
+    // // const balancePassManager = await BalanceVaultTemplate.attach("0x0993C3f7F1ADB0af8CBcbce0fa43Db5068edA991");
+    // const balancePassManager = await BalancePassManager.deploy();
+    // console.log(`Deployed BalancePassManager to: ${balancePassManager.address}`);
+    // console.log(`\nVerify:\nnpx hardhat verify --network ${network} ${balancePassManager.address}`);
+
+    const OffChainBalancePassHolderStrategy = await ethers.getContractFactory('OffChainBalancePassHolderStrategy');
+    // const offChainBalancePassHolderStrategy = await BalanceVaultTemplate.attach("0x629f17887785612Cd7C5cea6441c0869c0F96eCd");
+    const offChainBalancePassHolderStrategy = await OffChainBalancePassHolderStrategy.deploy();
+    console.log(`Deployed OffChainBalancePassHolderStrategy to: ${offChainBalancePassHolderStrategy.address}`);
+    console.log(`\nVerify:\nnpx hardhat verify --network ${network} ${offChainBalancePassHolderStrategy.address}`);
+
+
+
 }
 
 main()
