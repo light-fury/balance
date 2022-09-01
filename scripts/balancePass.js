0d9ee16dd09939a13956d6ce08e79b15a501c9b3
light-fury <leech.developer@gmail.com>
2022-09-01 07:12:23 +0200

    balance pass deploy
    Signed-off-by: pwntr0n <pwntr0n@fantohm.com>

--- scripts/balancePass.js
+++ scripts/balancePass.js
@@ -5,13 +5,13 @@ async function main() {
   console.log("Deploying contracts with the account: " + deployer.address);
 
   // const network = "rinkeby";
-  const network = "fantom";
+  const network = "mainnet";
 
   const maxMint = 350;
   const baseTokenURI = "ipfs://Qmc8A19qUxy1VWeSDtJj9cGk1DAfE88E47Xb5BFn5Z6Hg1";
   const passNFT = await ethers.getContractFactory("BalancePass");
   //const passnft = await passNFT.attach("0xa3DDAf083e491ecd5CbdCbd7DcC504cA7c0f2408"); // rinkeby
-  //const passnft = await passNFT.attach("0x9845bEec5b84697D3420A66EF20Bb052b55e1981"); // fantom
+  //const passnft = await passNFT.attach("0x2fd0ff45263143dcd616ecada45c0d22e49adbb7"); // mainnet
   const passnft = await passNFT.deploy(maxMint, baseTokenURI, true);
   console.log(`Deployed liqdnft to: ${passnft.address}`);
 
